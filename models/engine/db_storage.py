dd24fb3faaf9c0b0c76e9b87ce272d63f5879943
Julianeme <juliantve@gmail.com>
2021-08-11 22:58:03 -0500

    pep8 style correction

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,11 +6,10 @@ import models
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import session
-from sqlalchemy.orm.session import sessionmaker 
+from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.orm import scoped_session
 from models.base_model import BaseModel, Base
 from os import getenv
-
 
 
 class DBStorage():
