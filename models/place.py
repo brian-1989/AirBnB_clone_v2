b9cae2dc3e2a242a41db0ed909952dd28622427d
Julianeme <juliantve@gmail.com>
2021-08-11 22:26:41 -0500

    Property added for db using json

--- models/place.py
+++ models/place.py
@@ -10,17 +10,31 @@ from os import getenv
 
 class Place(BaseModel, Base):
     """ A place to stay """
-
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    # reviews = relationship('Review', backref='place',
-    #                        cascade=('all, delete'))
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        reviews = relationship('Review', backref='place',
+                               cascade=('all, delete'))
+    else:
+        @property
+        def reviews(self):
+            """
+            returns the list of Review instances with place_id equals to the
+            current Place.id
+            """
+            from models.review import Review
+            from models import storage
+            reviews_list = []
+            for rev in models.storage.all(Review).values():
+                if rev.place_id == self.id:
+                    reviews_list.append(rev)
+            return reviews_list
