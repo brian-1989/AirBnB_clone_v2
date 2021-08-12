db16b7a54604c49734fff70fc1879bc15fee04ed
Julianeme <juliantve@gmail.com>
2021-08-11 20:32:19 -0500

    conditioales for db selection removed

--- models/user.py
+++ models/user.py
@@ -9,20 +9,20 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'users'
-        # places = relationship('Place', backref="user", cascade="all, delete")
-        # class attribute places represents a relationship with the class Place
-        # If the User object is deleted, all linked Place objects will be
-        # automatically deleted. Also,the reference from a Place object to his
-        # User is named user
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        # reviews = relationship('Review', backref='user',cascade='all,delete')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+
+    __tablename__ = 'users'
+    # places = relationship('Place', backref="user", cascade="all, delete")
+    # class attribute places represents a relationship with the class Place
+    # If the User object is deleted, all linked Place objects will be
+    # automatically deleted. Also,the reference from a Place object to his
+    # User is named user
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    # reviews = relationship('Review', backref='user',cascade='all,delete')
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
