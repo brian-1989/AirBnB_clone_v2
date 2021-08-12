fbd2b532da67fdce3b577d829b1d724a3bb2455b
Julianeme <juliantve@gmail.com>
2021-08-11 22:28:03 -0500

    Foreing keys added

--- models/review.py
+++ models/review.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from os import getenv
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
