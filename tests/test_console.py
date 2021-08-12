8cd4d2f51ec8e0e9092fe6c2f08bb062450c6f27
Brian Zapata <dzapata397@gmail.com>
2021-08-12 00:01:42 -0500

    Add comments

--- tests/test_console.py
+++ tests/test_console.py
@@ -15,17 +15,29 @@ class test_console(unittest.TestCase):
     """Requirements cases
     """
     def test_to_the_module_docstring(self):
+        """ Test to the module docstring.
+
+        """
         self.assertTrue(len(console.__doc__) > 1)
 
     def test_to_the_class_docstring(self):
+        """ Test to the class.
+
+        """
         self.assertTrue(len(console.__doc__) > 1)
 
     def test_of_PEP8_console(self):
+        """ Test to the pep8.
+
+        """
         self.assertEqual(os.system("pep8 ./console.py"), 0)
 
     """ Console cases
     """
-    def test_to_console_do_create(self):
+    def test_to_console(self):
+        """ Test to the console.
+
+        """
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd('create State name="California"')
         self.assertIn('State'+'.'+f.getvalue()[:-1], storage.all())
