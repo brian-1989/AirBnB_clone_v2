ef04aa0df986c1ed8bfeeb0ed8b8b490d9319c4b
Julianeme <juliantve@gmail.com>
2021-08-11 21:30:25 -0500

    clases updated to deal with mysqlalchemy DB

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
 
 
 class City(BaseModel, Base):
@@ -12,4 +12,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
+    places = relationship('Place', backref='cities', cascade='all, delete')
--- models/user.py
+++ models/user.py
@@ -2,14 +2,14 @@
 """This module defines a class User"""
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-
     __tablename__ = 'users'
-    # places = relationship('Place', backref="user", cascade="all, delete")
+    places = relationship('Place', backref="user", cascade="all, delete")
     # class attribute places represents a relationship with the class Place
     # If the User object is deleted, all linked Place objects will be
     # automatically deleted. Also,the reference from a Place object to his
