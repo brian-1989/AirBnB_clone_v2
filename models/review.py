bf7fedc3b59e22a0b2d7d880dbac72d6c877b357
Brian Zapata <dzapata397@gmail.com>
2021-08-11 22:50:44 -0500

    Comments

--- models/review.py
+++ models/review.py
@@ -7,9 +7,11 @@ from sqlalchemy.sql.sqltypes import String
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """
+    Review classto store review information
+    """
 
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
