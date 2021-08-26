6ed0ef6f692379d94df52ed97828bd3b4cc2f33f
Brian Zapata <dzapata397@gmail.com>
2021-08-26 01:12:03 -0500

    add lines 8 to 10

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,6 +5,9 @@ then
     sudo apt-get -y update
     sudo apt-get -y install nginx
 fi
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
