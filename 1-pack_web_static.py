23c2733c2d899e5ad753116c46711a74d55757f2
Brian Zapata <dzapata397@gmail.com>
2021-08-26 19:57:06 -0500

    Delete ./ and file_tgz in the return

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,6 @@
 """ Compress a folder to a file.tgz.
 
 """
-
 from fabric.api import local
 from datetime import datetime
 import os
@@ -21,7 +20,7 @@ def do_pack():
     if not os.path.exists('versions'):
         os.makedirs('versions')
     # The path of file is organized
-    path_file = "./versions/web_static_{}.tgz".format(date)
+    path_file = "versions/web_static_{}.tgz".format(date)
     print("Packing web_static to {}".format(path_file))
     # Compress a folder to a file.tgz
     file_tgz = local("tar -czvf {} web_static".format(path_file))
@@ -31,4 +30,4 @@ def do_pack():
     else:
         size_file = os.path.getsize(path_file)
         print("web_static packed: {} -> {}Bytes".format(path_file, size_file))
-        return "./{}".format(file_tgz)
+        return "{}".format(path_file)
