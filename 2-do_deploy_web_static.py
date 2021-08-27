8e0abc6b95e8498f2caf45bc80190ed8bb129cb2
Brian Zapata <dzapata397@gmail.com>
2021-08-26 20:55:48 -0500

    changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,13 +26,13 @@ def do_deploy(archive_path):
         return False
     # Create the path to uncompress the file
     create_path = run(
-        "mkdir -p /data/web_static/releases/{}/".format(
+        "mkdir -p /data/web_static/releases/{}".format(
             file_name[0]))
     if create_path.failed:
         return False
     # probar con create_path
     uncompress_file = run(
-        "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+        "tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
             file_name_tgz[1], file_name[0]))
     if uncompress_file.failed:
         return False
@@ -43,13 +43,13 @@ def do_deploy(archive_path):
     # Mover the file
     move_file = run(
         "mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}".format(
+            /data/web_static/releases/{}/".format(
                 file_name[0], file_name[0]))
     if move_file.failed:
         return False
     # Delete symbolic_link
     delete_path = run(
-        "rm -rf /data/web_static/releases/{}/web_static/".format(
+        "rm -rf /data/web_static/releases/{}/web_static".format(
             file_name[0]))
     if delete_path.failed:
         return False
@@ -58,7 +58,7 @@ def do_deploy(archive_path):
         return False
     # Create new symbolic_link
     new_symbolic_link = run(
-        "ln -s -f /data/web_static/releases/{}\
+        "ln -s /data/web_static/releases/{}\
             /data/web_static/current".format(file_name[0]))
     if new_symbolic_link.failed:
         return False
