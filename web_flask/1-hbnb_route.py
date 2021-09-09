88924a14b5b446739e77bb1b30e3d53252261b99
Brian Zapata <dzapata397@gmail.com>
2021-09-09 17:25:31 -0500

    The HBNB function was created

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Flask web application.
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ This function start an application in an address URL '/'.
+    Return: Hello HBNB!.
+
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ This function start an application in an address URL '/hbnb'.
+    Return: HBNB.
+
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
