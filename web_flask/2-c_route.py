94aa232445b3f3730850d6baefd0fafe0d6efb37
Brian Zapata <dzapata397@gmail.com>
2021-09-09 17:55:15 -0500

    The C function was created

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Flask web application.
+
+"""
+
+from os import replace
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ This function start an application in an address URL '/'.
+    Return: Hello HBNB!.
+
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ This function start an application in an address URL '/hbnb'.
+    Return: HBNB.
+
+    """
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    """ This function start an application in an address URL '/c/<text>'.
+    The 'text' variable, is a variable that is passed from browser together with the URL.
+    Return: C and the value of the text variable.
+
+    """
+    without_underscore = escape(text).replace('_', ' ')
+    return "C {}".format(without_underscore)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
