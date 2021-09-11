9a8d636fb1085e2d58e7d8fa15e9aa703af92b4e
Brian Zapata <dzapata397@gmail.com>
2021-09-10 23:47:54 -0500

    The teardown method was created

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,16 +11,22 @@ app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-@app.teardown_appcontext
 def states_list():
     """ This function start an application in an address URL
     '/states_list'.
     Return: An Html page with the states.
 
     """
-    storage.close()
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown(error):
+    """ This method after each request you must remove
+    the current SQLAlchemy Session.
+
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
