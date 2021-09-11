1094f32c25d51e5790ef129c9a6445a25fef84f8
Brian Zapata <dzapata397@gmail.com>
2021-09-11 18:32:01 -0500

    The states, states_id and teardown method was created

---
+++ web_flask/9-states.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Flask web application.
+
+"""
+
+from flask import Flask, render_template, escape
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """ This function start an application in an address URL
+    '/states'.
+    Return: An Html page with the states.
+
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """ This function start an application in an address URL
+    '/states/<id>'.
+    The 'id' variable, is a variable that is passed from browser
+    together with the URL.
+    Return: An Html page with the states.
+
+    """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    for i in cities:
+        if escape(id) == i.state_id:
+            my_flag = True
+            return render_template(
+                "9-states.html",
+                states=states, cities=cities, id=escape(id), my_flag=my_flag)
+    my_flag = False
+    return render_template("9-states.html", id=my_flag)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """ This method after each request you must remove
+    the current SQLAlchemy Session.
+
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
